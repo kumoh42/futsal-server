fcae3715767161f9444037f44727b5c5bc383035
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-07-13 20:09:21 +0900

    chore: add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: "mysql",
       host: process.env.DATABASE_HOST,
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from "dotenv"
+import * as path from "path"
+
+dotenv.config({
+  path: path.resolve('.env')
+})
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
