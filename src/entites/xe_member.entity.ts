ded22e8c102d0cb81f47cc0d342bfe7953339673
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-07-24 14:00:07 +0900

    style: npm run format 실행

--- src/entites/xe_member.entity.ts
+++ src/entites/xe_member.entity.ts
@@ -1,52 +1,49 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('xe_member')
-export class Xe_MemberEntity{
-    
-    /**
-     * xe_member - member_srl
-     * @description xe_member pk 입니다. 
-     * 
-     * PK, NN, AI
-     */    
-    @PrimaryGeneratedColumn()
-    member_srl : number;
+export class Xe_MemberEntity {
+  /**
+   * xe_member - member_srl
+   * @description xe_member pk 입니다.
+   *
+   * PK, NN, AI
+   */
+  @PrimaryGeneratedColumn()
+  member_srl: number;
 
-    /**
-     * xe_member - user_id
-     * @description 사용자의 id 입니다. 
-     * 
-     * NN,UQ
-     */    
-    @Column({length: 80})
-    user_id : string;
-    
+  /**
+   * xe_member - user_id
+   * @description 사용자의 id 입니다.
+   *
+   * NN,UQ
+   */
+  @Column({ length: 80 })
+  user_id: string;
 
-    /**
-     * xe_member - password
-     * @description 사용자의 비밀번호 입니다. 
-     * 
-     * NN
-     */    
-    @Column({length: 60})
-    password : string;
+  /**
+   * xe_member - password
+   * @description 사용자의 비밀번호 입니다.
+   *
+   * NN
+   */
+  @Column({ length: 60 })
+  password: string;
 
-    /**
-     * xe_member - user_name
-     * @description 사용자의 실명입니다.
-     * 
-     * NN
-     */    
-    @Column({length: 40})
-    user_name : string;
-    
-    /**
-     * xe_member - nick_name
-     * @description 사용자 계정의 닉네임입니다. 
-     * 
-     * NN, UQ
-     */    
-    @Column({length: 40})
-    nick_name : string;
+  /**
+   * xe_member - user_name
+   * @description 사용자의 실명입니다.
+   *
+   * NN
+   */
+  @Column({ length: 40 })
+  user_name: string;
 
+  /**
+   * xe_member - nick_name
+   * @description 사용자 계정의 닉네임입니다.
+   *
+   * NN, UQ
+   */
+  @Column({ length: 40 })
+  nick_name: string;
 }
--- src/entites/xe_reservation_member.entity.ts
+++ src/entites/xe_reservation_member.entity.ts
@@ -1,85 +1,75 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('xe_reservation_member')
-export class Xe_Reservation_MemberEntity{
-    
+export class Xe_Reservation_MemberEntity {
+  /**
+   * xe_reservation_member - member_srl
+   * @description member pk입니다. xe_member pk와 value가 동일합니다.
+   *
+   * Pk, NN
+   */
+  @PrimaryGeneratedColumn()
+  member_srl: number;
 
-    /**
-     * xe_reservation_member - member_srl
-     * @description member pk입니다. xe_member pk와 value가 동일합니다.
-     * 
-     * Pk, NN
-     */    
-    @PrimaryGeneratedColumn()
-    member_srl : number;
+  /**
+   * xe_reservation_member - major_srl
+   * @description major pk입니다. xe_reservation_major pk와 value가 동일합니다.
+   *
+   */
+  @Column()
+  major_srl: number;
 
+  /**
+   * xe_reservation_member - circle_srl
+   * @description circle pk입니다. xe_reservation_circle pk와 value가 동일합니다.
+   *
+   * Default - NULL
+   */
+  @Column()
+  circle_srl: number;
 
-    /**
-     * xe_reservation_member - major_srl
-     * @description major pk입니다. xe_reservation_major pk와 value가 동일합니다.
-     * 
-     */    
-    @Column()
-    major_srl : number;
+  /**
+   * xe_reservation_member - user_name
+   * @description 예약멤버의 이름을 뜻합니다.
+   *
+   * NN
+   */
+  @Column({ length: 20 })
+  user_name: string;
 
+  /**
+   * xe_reservation_member - phone_number
+   * @description 예약멤버의 전화번호입니다.
+   *
+   * Default - NULL
+   */
+  @Column({ length: 20 })
+  phone_number: string;
 
-    /**
-     * xe_reservation_member - circle_srl
-     * @description circle pk입니다. xe_reservation_circle pk와 value가 동일합니다.
-     * 
-     * Default - NULL
-     */    
-    @Column()
-    circle_srl : number;
+  /**
+   * xe_reservation_member - is_denied
+   * @description ???
+   *
+   * Default - 'Y'
+   */
+  @Column({ length: 1 })
+  is_denied: string;
 
+  /**
+   * xe_reservation_member - permission
+   * @description 예약멤버의 유형을 뜻합니다. 'user', 'admin' 두 종류가 존재합니다.
+   *
+   * Default - 'user'
+   */
+  @Column({ length: 10 })
+  permission: string;
 
-    /**
-     * xe_reservation_member - user_name
-     * @description 예약멤버의 이름을 뜻합니다. 
-     * 
-     * NN
-     */        
-    @Column({length: 20})
-    user_name : string;
-    
-
-    /**
-     * xe_reservation_member - phone_number
-     * @description 예약멤버의 전화번호입니다.
-     * 
-     * Default - NULL
-     */    
-    @Column({length: 20})
-    phone_number : string;
-
-
-    /**
-     * xe_reservation_member - is_denied
-     * @description ???
-     * 
-     * Default - 'Y'
-     */    
-    @Column({length: 1})
-    is_denied : string;
-
-
-    /**
-     * xe_reservation_member - permission
-     * @description 예약멤버의 유형을 뜻합니다. 'user', 'admin' 두 종류가 존재합니다.
-     * 
-     * Default - 'user'
-     */  
-    @Column({length: 10})
-    permission : string;
-
-
-    /**
-     * xe_reservation_member - regdate
-     * @description 등록일자? 일걸요? ...ㅎㅎ
-     * 
-     * Default - CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP()
-     */  
-    @Column({length: 60})
-    regdate : string;
-
+  /**
+   * xe_reservation_member - regdate
+   * @description 등록일자? 일걸요? ...ㅎㅎ
+   *
+   * Default - CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP()
+   */
+  @Column({ length: 60 })
+  regdate: string;
 }
