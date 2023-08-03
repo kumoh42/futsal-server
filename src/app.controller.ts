da7ce5ffdb9a8f2ea92b6e7b293f36ea3b5c056f
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-08-03 10:15:03 +0900

    test: fix path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/test3')
+  @Get('/test')
   getHello(): string {
     return this.appService.getHello();
   }
