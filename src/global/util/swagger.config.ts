b4770938fdc5164e65e3d55e34958e78114ed059
hyunoi <nhe0622@gmail.com>
2023-08-14 18:16:30 +0900

    Title, Description 변경

--- src/global/util/swagger.config.ts
+++ src/global/util/swagger.config.ts
@@ -8,8 +8,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
  */
 export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
-    .setTitle('NestJS Study API Docs') // Document의 제목
-    .setDescription('NestJS Study API description') // 제목 하단에 기재될 설명
+    .setTitle('Futsal Server api Docs') // Document의 제목
+    .setDescription('Futsal Server에 사용되는 API들이 기재된 문서입니다.') // 제목 하단에 기재될 설명
     .setVersion('1.0.0') // 버전 (개발 -> 0)
     .build();
 
