f350109aad8fbb45efb8b0f423416028564e41d7
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-08-16 19:23:55 +0900

    Fix: bootstrap logic and add direct execution check

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from 'src/global/util/swagger.config'
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './config/swagger.config';
 
 dotenv.config({
   path: path.resolve('.env'),
 });
 
-async function bootstrap() {
+export async function bootstrap() {
   console.log(process.env);
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
@@ -26,6 +26,15 @@ async function bootstrap() {
   );
 
   setupSwagger(app);
+  
+  return app;
+}
+
+async function main() {
+  const app = await bootstrap();
   await app.listen(3000);
 }
-bootstrap();
+
+if (require.main === module) {
+  main();
+}
