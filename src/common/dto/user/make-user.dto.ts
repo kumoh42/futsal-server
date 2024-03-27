45f2ec861b4f6576c542053086ebcd39b803de71
juhyeok <kyoung1678@naver.com>
2024-03-27 16:41:41 +0900

    Fix: changed password hassing && validation

--- src/common/dto/user/make-user.dto.ts
+++ src/common/dto/user/make-user.dto.ts
@@ -28,5 +28,8 @@ export class NewUserDto {
   
   @IsString()
   @IsNotEmpty()
+  @Matches(/^(?=.*[a-z])(?=.*[\d!@#$%^&*()])[a-zA-Z\d!@3$%^7*()]{8,20}$/,{
+    message:'패스워드는 8자 이상 20자 이하 소문자,영어,숫자가 포함되어야 합니다.'
+  }) 
   password: string;
 }
