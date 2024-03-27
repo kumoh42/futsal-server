a82248af8a81daa7c4438a1103c76feda2182157
hyunoi <nhe0622@gmail.com>
2024-03-27 14:42:17 +0900

    feat: login (token 제외)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,26 +1,28 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from '@/auth/jwt/jwt.guard';
 import { User } from '@/common/decorators/user.decorator';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { NewUserDto } from '@/common/dto/user/make-user.dto';
+import { CreateUserDto } from '@/auth/dto/create-user.dto';
 
 @ApiTags('유저 정보 조회')
 @Controller('user')
+@UseGuards(JwtAuthGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
   @ApiOperation({ description: '로그인한 유저의 정보를 조회' })
-  @UseGuards(JwtAuthGuard)
   async getUserInfo(@User() user: User) {
     const { userId } = user;
     return await this.userService.getUserInfo(userId);
   }
 
   @Post()
-  async makeUser(@Body() info: NewUserDto){
-    return await this.userService.makeNewUser(info);
+  async login(@Body() body: CreateUserDto, @Res() response: Response) {
+    const [access_token, refresh_token] = await this.userService.login(
+      body.user_id,
+      body.user_password,
+    );
   }
-  
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,16 @@
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Xe_Member_FutsalEntity } from '@/entites/xe_member.futsal.entity';
-import { Repository } from 'typeorm';
-import { NewUserDto } from '@/common/dto/user/make-user.dto';
 import { Xe_Reservation_MemberEntity } from '@/entites/xe_reservation_member.entity';
+import { Repository } from 'typeorm';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(Xe_Member_FutsalEntity)
     private userRepository: Repository<Xe_Member_FutsalEntity>,
-    @InjectRepository(Xe_Reservation_MemberEntity)
-    private memberRepository: Repository<Xe_Reservation_MemberEntity>,
+    private memberRepository: Repository<Xe_Reservation_MemberEntity>
   ) {}
 
   async getUserInfo(userId: string): Promise<any> {
@@ -29,46 +28,23 @@ export class UserService {
     return userInfoWithoutPassword;
   }
 
+  async login(userId: string, userPassword: string) {
+    const member = await this.memberRepository.findOne({
+      where: { user_id: userId }
+    });
 
-  async makeNewUser(info: NewUserDto): Promise<string>{
-
-    const existingUserById = await this.memberRepository.findOne({
-      where: {user_id: info.id}
-    })
-    const existingUserBySNumber = await this.memberRepository.findOne({ 
-      where:{user_student_number: info.sNumber}
-     });
-
-    if (existingUserById) {
-        throw new BadRequestException('이미 존재하는 사용자 ID입니다.');
-    }
-    if (existingUserBySNumber) {
-        throw new BadRequestException('이미 존재하는 학번입니다.');
+    if (!member) {
+      throw new NotFoundException(['아이디가 존재하지 않습니다.']);
     }
 
-    try{
-      const newUser = this.memberRepository.create({
-        user_id: info.id,
-        user_password: info.password,
-        user_name: info.name,
-        user_student_number: info.sNumber,
-        major_srl: info.major,
-        circle_srl: info.circle,
-        phone_number: info.phoneNumber,
-      })
+    const passwordCompareResult = await compare(userPassword, member.user_password);
 
-      await this.memberRepository.save(newUser);
-      return info.name + ' 학우님  회원가입 완료';
-    }catch(error){
-      console.log(error)
-      throw new BadRequestException('알 수 없는 에러가 발생했습니다.')
+    if (!passwordCompareResult) {
+      throw new NotFoundException(['잘못된 비밀번호 입니다.']);
     }
-}
+    
+    const a = 0, b = 0;
 
-  //password 해시 생성 알고리즘 입니다 ! 후에 있을 실제 디비 연동을 위해 일단 주석처리 해놓았습니다.
-  // async hashPassword(password: string): Promise<string> {
-  //   const saltRounds = 10; // 솔트를 사용하여 해싱할 횟수 (추가 보안)
-  //   const hashedPassword = await hash(password, saltRounds);
-  //   return hashedPassword;
-  // }
+    return [a, b]
+  }
 }
