59d9cf1d28ddf18d4a876177b8ef0424e44932e0
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2024-03-31 02:06:23 +0900

    Feat: add CheckPermission Guard for user,admin

---
+++ src/common/decorators/permission.guard.ts
@@ -0,0 +1,35 @@
+import { CanActivate, ExecutionContext, Injectable, Type } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ModuleRef } from '@nestjs/core';
+
+@Injectable()
+export class PermissionsGuard implements CanActivate {
+  private permission: string ='';
+
+  constructor(private reflector: Reflector, private moduleRef: ModuleRef) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    console.log(user)
+    console.log(this.permission)
+    return user.permission === this.permission
+  }
+  
+  setPermissions(permission: string) {
+    this.permission = permission;
+  }
+}
+
+// Guard 생성을 위한 팩토리 함수
+export function CheckPermissions(permission: string): Type<CanActivate> {
+  @Injectable()
+  class MixinPermissionsGuard extends PermissionsGuard {
+    constructor(reflector: Reflector, moduleRef: ModuleRef) {
+      super(reflector, moduleRef);
+      this.setPermissions(permission);
+    }
+  }
+
+  return MixinPermissionsGuard;
+}
